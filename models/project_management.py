764451910c4fd5257b65babb25d25d40abae14a1
Ebraheem <ebraheem20012020@gmail.com>
2024-10-20 14:05:00 +0300

    add compute progress method

--- models/project_management.py
+++ models/project_management.py
@@ -1,3 +1,4 @@
+from datetime import date
 from odoo import api, models, fields
 from odoo.http import request
 
@@ -21,6 +22,18 @@ class ProjectManagement(models.Model):
     )
     assigned_team = fields.Many2many('hr.employee')
     tasks_ids = fields.One2many('project.task', 'project_id')
+    progress = fields.Float(string='Progress', compute='_compute_progress')
+
+    @api.depends('start_date', 'end_date')
+    def _compute_progress(self):
+        today = date.today()
+        for record in self:
+            if record.start_date and record.end_date:
+                total_duration = (record.end_date - record.start_date).days
+                elapsed_duration = (today - record.start_date).days
+                record.progress = max(0, min(100, (elapsed_duration / total_duration) * 100)) if total_duration > 0 else 0
+            else:
+                record.progress = 0
 
     def action_generate_excel_report(self):
         return {
